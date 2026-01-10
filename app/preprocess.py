60e8e9ad611ec39226b90f1d9e9c587da12f5f98
Arindam Das <146046146+ArindamDas07@users.noreply.github.com>
2026-01-10 16:33:06 +0530

    Delete app directory

--- app/preprocess.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import numpy as np
-
-def preprocess_image(image):
-    image = image.resize((32, 32))
-    arr = np.array(image) / 255.0
-    return np.expand_dims(arr, axis=0)
